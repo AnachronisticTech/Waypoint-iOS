ef5d6c95f8405759bd9e6899185de08e1e8f830f
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2019-07-27 18:57:23 +0100

    Initial Commit

---
+++ Waypoint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Waypoint
+//
+//  Created by Daniel Marriner on 27/07/2019.
+//  Copyright © 2019 Daniel Marriner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
